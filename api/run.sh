33042a4e4b345c0f5105ee6a202bde04fcf7ad4b
Amani215 <amanibrik5@gmail.com>
2022-09-11 22:48:21 +0000

    Removed: unnecessary library

--- api/run.sh
+++ api/run.sh
@@ -1,7 +1,5 @@
 apk add build-base
 
-apk add --no-cache postgresql-libs
-
 apk add --no-cache --virtual .build-deps gcc musl-dev postgresql-dev
 
 python3 -m pip install -r requirements.txt && python3 -m flask run --host=0.0.0.0

5d46778fdcbe2096e0854244ecd7d7b0cc9c24d7
Amani215 <amanibrik5@gmail.com>
2022-09-15 19:35:55 +0000

    Updated: app factory

--- api/app.py
+++ api/app.py
@@ -2,12 +2,12 @@
 import os.path
 from dotenv import load_dotenv
 from flask import Flask
-from model import db
-from route.user import user_api
-from route.auth import auth_api
 
 def create_app():
-    """Create the app"""
+    """Create the app 
+    
+    (Application factory: https://flask.palletsprojects.com/en/2.2.x/patterns/appfactories/)
+    """
     app = Flask(__name__)
 
     load_dotenv()
@@ -15,17 +15,23 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
     app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
 
+    from model import db
     db.init_app(app)
+    setup_database(db, app)
+    
+    from route.user import user_api
+    from route.auth import auth_api
     app.register_blueprint(user_api)
     app.register_blueprint(auth_api)
+    
     return app
 
-def setup_database(_app):
+def setup_database(db, _app):
     """Create the postgres database"""
     with _app.app_context():
         # db.drop_all()
         db.create_all()
+        return db
 
 _app = create_app()
-setup_database(_app)
 _app.run()

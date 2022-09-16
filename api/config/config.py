da131485651a4566ecbd98dc9d4ab150fec5d288
Amani215 <amanibrik5@gmail.com>
2022-09-16 13:45:10 +0000

    Added: config file

---
+++ api/config/config.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+
+# uncomment the line below for postgres database url from environment variable
+# postgres_local_base = os.environ['DATABASE_URL']
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_DEV')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS_DEV')
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_TEST')
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS_TEST')
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig
+)
+
+key = Config.SECRET_KEY

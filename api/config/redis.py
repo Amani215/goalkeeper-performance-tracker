9fb5605244a0229f74e029d4e08f66e15348f53a
Amani215 <amanibrik5@gmail.com>
2022-10-02 19:14:14 +0000

    Changed: force redis port to int

--- api/config/redis.py
+++ api/config/redis.py
@@ -2,6 +2,6 @@ import redis
 import os
 
 redis_host = os.getenv("REDIS_HOST")
-redis_port = os.getenv("REDIS_PORT")
+redis_port:int = os.getenv("REDIS_PORT")
 
-redis_db = redis.Redis(host=redis_host, port=int(redis_port), db=0)
+redis_db = redis.Redis(host=redis_host, port=redis_port, db=0)

fe18a8da4acc2f2a36f76c0f81b0eaa1f5bfe219
Amani215 <amanibrik5@gmail.com>
2022-10-02 18:42:10 +0000

    Changed: redis port passed as string

--- api/config/redis.py
+++ api/config/redis.py
@@ -4,4 +4,4 @@ import os
 redis_host = os.getenv("REDIS_HOST")
 redis_port = os.getenv("REDIS_PORT")
 
-redis_db = redis.Redis(host=redis_host, port=redis_port, db=0)
+redis_db = redis.Redis(host=redis_host, port=str(redis_port), db=0)

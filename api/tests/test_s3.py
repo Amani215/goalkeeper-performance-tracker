c479183b567ee3075f89d5772d8694ff50cab87e
Amani215 <amanibrik5@gmail.com>
2022-10-07 20:51:01 +0000

    Fixed: test upload file

--- api/tests/test_s3.py
+++ api/tests/test_s3.py
@@ -1,10 +1,11 @@
 """Testing S3 services"""
+from werkzeug.datastructures import FileStorage
 from service.s3 import upload_file
 
 def test_file_upload(bucket):
     """Test if the file gets uploaded successfully"""
     
     with open('tests/assets/image.jpeg', 'rb')as f:
-        upload_file(f, bucket.name, 'test.jpeg')
+        upload_file(FileStorage(f), bucket.name)
         
     assert sum(1 for _ in bucket.objects.all()) == 1

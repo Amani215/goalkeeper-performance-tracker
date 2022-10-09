21fb2c067eedce62d0e29f78c4fe51d1c65946be
Amani215 <amanibrik5@gmail.com>
2022-10-09 22:54:51 +0000

    Changed: replace " with '

--- api/route/user.py
+++ api/route/user.py
@@ -1,4 +1,4 @@
-"""User routes (get, post, etc.)"""
+'''User routes (get, post, etc.)'''
 from flask import jsonify, request
 from flask.blueprints import Blueprint
 import service.user as user_service
@@ -11,11 +11,11 @@ NO_DATA_PROVIDED_MESSAGE = 'No data was provided'
 
 @user_api.route('/user', methods=['POST'])
 def add_user():
-    """Add a new user
+    '''Add a new user
 
     Takes a username and a password and returns the new user ID.
     If the username already exists the user is not added.
-    """
+    '''
     try:
         if not request.json:
             raise ValueError(NO_DATA_PROVIDED_MESSAGE)
@@ -26,17 +26,17 @@ def add_user():
         user_response = user_service.add_user(username=username, password=password, admin=admin)
         return user_response, 201
     except Exception as err:
-        return {"error":str(err)}, 400
+        return {'error':str(err)}, 400
   
 @user_api.route('/user', methods = ['GET'])
 @token_required(admin=False)
 def get_users(current_user:User):
-    """Get all requested users
+    '''Get all requested users
 
     If id is provided then then only the user with that id is returned
     If a username is provided then only the user with that username is returned
     If nothing is provided then all the users are returned
-    """
+    '''
     try:
         if 'id' in request.json:
             user = user_service.get_by_id(request.json['id'])
@@ -48,16 +48,16 @@ def get_users(current_user:User):
         
         return user.serialize
     except PermissionError as err:
-        return {"error":str(err)}, 401
+        return {'error':str(err)}, 401
     except Exception as err:
-        return {"error":str(err)}, 400
+        return {'error':str(err)}, 400
 
 @user_api.route('/user/admin', methods = ['PUT'])
 @token_required(admin=True)
 def set_admin(current_user: User):
-    """Update the admin status of a given user
+    '''Update the admin status of a given user
     
-    Only the admin has this right and the admin's default status cannot be changed"""
+    Only the admin has this right and the admin's default status cannot be changed'''
     
     try: 
         if not request.json:
@@ -69,7 +69,7 @@ def set_admin(current_user: User):
         return user_response, 200
     
     except PermissionError as err:
-        return {"error":str(err)}, 401
+        return {'error':str(err)}, 401
     except Exception as err:
         return {'error': str(err)}, 400
 
@@ -88,9 +88,9 @@ def add_category(current_user:User):
         # user_service.add_category(trainer, category)
         
     except PermissionError as err:
-        return {"error":str(err)}, 401
+        return {'error':str(err)}, 401
     except Exception as err:
-        return {"error":str(err)}, 400
+        return {'error':str(err)}, 400
 
 @user_api.route('/user/category', methods=['DELETE'])
 @token_required(admin=True)
@@ -107,27 +107,27 @@ def remove_category(current_user:User):
         # user_service.remove_category(trainer, category)
         
     except PermissionError as err:
-        return {"error":str(err)}, 401
+        return {'error':str(err)}, 401
     except Exception as err:
-        return {"error":str(err)}, 400
+        return {'error':str(err)}, 400
     
 @user_api.route('/user/profile_pic', methods=['PUT'])
 @token_required(admin=False)
 def add_profile_pic(current_user:User):
-    """Add a profile pic for the user
+    '''Add a profile pic for the user
     
     A file has to be passed in request.files
     A token is required for the user to be able to upload the image
-    The image is automatically uploaded to the logged in user"""
+    The image is automatically uploaded to the logged in user'''
     try:
-        if request.files.get("profile_pic") is None:
+        if request.files.get('profile_pic') is None:
             raise ValueError(NO_DATA_PROVIDED_MESSAGE)
         
-        pic = request.files["profile_pic"]
+        pic = request.files['profile_pic']
         pic_url = user_service.update_profile_pic(current_user, pic)
-        return {"url": pic_url}
+        return {'url': pic_url}
         
     except PermissionError as err:
-        return {"error":str(err)}, 401
+        return {'error':str(err)}, 401
     except Exception as err:
-        return {"error":str(err)}, 400
+        return {'error':str(err)}, 400

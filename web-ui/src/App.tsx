8a02688f4fa3d144c38c0acbb8d479f8598142d4
Amani215 <amanibrik5@gmail.com>
2022-10-09 00:27:00 +0000

    Added: Sign in component

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,29 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignInSide from './pages/landing/login';
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): JSX.Element {
+  return <SignInSide/>
 }
 
 export default App;

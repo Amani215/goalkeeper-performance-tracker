7d8d752db156dc3f0e75d63709feed2b82853a7b
Amani215 <amanibrik5@gmail.com>
2022-10-16 22:27:59 +0000

    Added: category blueprint registered

--- api/app.py
+++ api/app.py
@@ -24,9 +24,11 @@ def create_app():
     
     from route.user import user_api
     from route.auth import auth_api
+    from route.category import category_api
     app.register_blueprint(user_api)
     app.register_blueprint(auth_api)
-
+    app.register_blueprint(category_api)
+    
     return app
 
 def setup_database(_db, _app):

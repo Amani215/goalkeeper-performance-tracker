7b494cc22a464d472abdd425334793dda8055ad4
Amani215 <amanibrik5@gmail.com>
2022-10-30 17:06:19 +0000

    Update: Center app name

--- web-ui/src/components/header/index.tsx
+++ web-ui/src/components/header/index.tsx
@@ -2,10 +2,9 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import { MdMenu } from "react-icons/md";
 import AccountPopover from './AccountPopover';
 import { useUser } from '../../contexts/userContext';
+import { useSideNavIsOpen } from '../../contexts/pageContext';
 
 export default function Header() {
   const user = useUser()
@@ -14,20 +13,8 @@ export default function Header() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MdMenu />
-          </IconButton>
-
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            GPT
-          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>GPT</Typography>
 
           <div>
             <AccountPopover username={user ? user.username : ""}

29854fc3ce0db57980d4c4d488813daad366fbf3
Amani215 <amanibrik5@gmail.com>
2022-10-30 21:01:21 +0000

    Added: users page

---
+++ web-ui/src/pages/users/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PortalPage from '../../containers/portalPage'
+
+const Users = () => {
+  return (
+    <PortalPage>
+      Users
+    </PortalPage>
+  )
+}
+
+export default Users
--- web-ui/src/router/index.tsx
+++ web-ui/src/router/index.tsx
@@ -1,13 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Dashboard from '../pages/dashboard';
 import Login from '../pages/login';
+import Users from '../pages/users';
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/" element={<Dashboard />} />
             </Routes>
         </Router>
 

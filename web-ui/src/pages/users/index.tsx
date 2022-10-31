76c89a1a94ea23c34e62bad2cd6f500d3cb41341
Amani215 <amanibrik5@gmail.com>
2022-10-31 14:35:23 +0000

    Added: users list

--- web-ui/src/pages/users/index.tsx
+++ web-ui/src/pages/users/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import PortalPage from '../../containers/portalPage'
+import UsersList from '../../containers/usersList'
 
 const Users = () => {
   return (
     <PortalPage>
-      Users
+      <UsersList />
     </PortalPage>
   )
 }

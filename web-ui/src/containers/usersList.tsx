bae4afd415d9ac939080f6547894825441ea1021
Amani215 <amanibrik5@gmail.com>
2022-11-01 14:13:18 +0000

    Update: format users table

--- web-ui/src/containers/usersList.tsx
+++ web-ui/src/containers/usersList.tsx
@@ -2,12 +2,41 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
 import { useUsers, useUsersReady } from '../contexts/usersContext';
 import { UserDTO } from '../DTOs';
+import Avatar from '@mui/material/Avatar';
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', flex: 1, minWidth: 100 },
-    { field: 'username', headerName: 'Username', flex: 2, minWidth: 100 },
-    { field: 'admin', headerName: 'Admin', flex: 1, minWidth: 80 },
-    { field: 'profile_pic', headerName: 'Profile Pic', flex: 2, minWidth: 100 }
+    {
+        field: 'profile_pic',
+        headerName: 'Profile Pic',
+        flex: 1,
+        minWidth: 60,
+        align: "center",
+        renderCell: (params) => {
+            return (
+                <>
+                    <Avatar src={params.value} sx={{ width: 32, height: 32 }} />
+                </>
+            );
+        }
+    },
+    {
+        field: 'id',
+        headerName: 'ID',
+        flex: 2,
+        minWidth: 80
+    },
+    {
+        field: 'username',
+        headerName: 'Username',
+        flex: 3, minWidth: 100
+    },
+    {
+        field: 'admin',
+        headerName: 'Admin',
+        flex: 1,
+        type: 'boolean',
+        minWidth: 80
+    }
 ];
 
 export default function UsersList() {

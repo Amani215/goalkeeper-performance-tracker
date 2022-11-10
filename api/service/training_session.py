20305baf73350ef0ace266fe8d1b3b485013b0d3
Amani215 <amanibrik5@gmail.com>
2022-11-10 19:15:01 +0000

    Added: training session service (not tested)

---
+++ api/service/training_session.py
@@ -0,0 +1,44 @@
+'''Training session services (add, update, etc.)'''
+from datetime import datetime
+from sqlalchemy.exc import SQLAlchemyError
+from config.postgres import db
+from model.training_session import training_session
+import service.category as category_service
+
+
+def add_training_session(date: str, duration: int, category_id: str):
+    '''Add a new training session to the database'''
+    training_session_date = datetime.strptime(date, '%d/%m/%Y')
+    category = category_service.get_by_id(category_id)
+    training_session_obj = training_session(training_session_date, duration,
+                                            category)
+
+    db.session.add(training_session_obj)
+    db.session.commit()
+
+    return training_session_obj
+
+
+def get_training_sessions():
+    '''Get all training sessions'''
+    return training_session.query.all()
+
+
+def get_by_id(id):
+    '''Get training session by ID'''
+    try:
+        training_session_obj: training_session = training_session.query.filter_by(
+            id=id).one()
+        return training_session_obj
+    except SQLAlchemyError as err:
+        return {'error': str(err)}
+
+
+def update_category(training_session_id: str, category_id: str):
+    '''Update the category of the training session'''
+    training_session_obj = get_by_id(training_session_id)
+    category = category_service.get_by_id(category_id)
+
+    training_session_obj.training_session_category = category
+    db.session.commit()
+    return training_session_obj

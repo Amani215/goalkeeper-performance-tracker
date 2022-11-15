56fc95616365807e862773f620ef61023b6e3959
Amani215 <amanibrik5@gmail.com>
2022-11-15 23:55:01 +0000

    Update: match performances position

--- web-ui/src/containers/goalkeeperDetails.tsx
+++ web-ui/src/containers/goalkeeperDetails.tsx
@@ -151,163 +151,165 @@ function GoalkeeperDetails() {
                     {error}
                 </Typography>
                 :
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    <Grid item xs={4} sm={5} md={8} order={{ xs: 2, sm: 1, md: 1 }}>
-                        <Card sx={{ padding: 2 }}>
-                            <Box display="flex" justifyContent="flex-end">
-                                <Button>
-                                    Edit
-                                </Button>
-                            </Box>
+                <>
+                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        <Grid item xs={4} sm={5} md={8} order={{ xs: 2, sm: 1, md: 1 }}>
+                            <Card sx={{ padding: 2, height: "100%" }}>
+                                <Box display="flex" justifyContent="flex-end">
+                                    <Button>
+                                        Edit
+                                    </Button>
+                                </Box>
 
-                            <Grid container columns={8}>
-                                <Grid item xs={3}>
-                                    <Typography
-                                        variant='subtitle1'
-                                        sx={{ fontWeight: 'bold' }}>
-                                        Name
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography
-                                        variant='body1'>
-                                        {goalkeeper?.name}
-                                    </Typography>
-                                </Grid>
+                                <Grid container columns={8}>
+                                    <Grid item xs={3}>
+                                        <Typography
+                                            variant='subtitle1'
+                                            sx={{ fontWeight: 'bold' }}>
+                                            Name
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography
+                                            variant='body1'>
+                                            {goalkeeper?.name}
+                                        </Typography>
+                                    </Grid>
 
-                                <Grid item xs={3}>
-                                    <Typography
-                                        variant='subtitle1'
-                                        sx={{ fontWeight: 'bold' }}>
-                                        Age
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography
-                                        variant='body1'>
-                                        {dayjs().diff(dayjs(goalkeeper?.birthday), 'year')}
-                                    </Typography>
-                                </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography
+                                            variant='subtitle1'
+                                            sx={{ fontWeight: 'bold' }}>
+                                            Age
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography
+                                            variant='body1'>
+                                            {dayjs().diff(dayjs(goalkeeper?.birthday), 'year')}
+                                        </Typography>
+                                    </Grid>
 
-                                <Grid item xs={3}>
-                                    <Typography
-                                        variant='subtitle1'
-                                        sx={{ fontWeight: 'bold' }}
-                                        mr={2}>
-                                        Birthday
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography
-                                        variant='body1'>
-                                        {dayjs(goalkeeper?.birthday).format('DD MMM YYYY').toString()}
-                                    </Typography>
-                                </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography
+                                            variant='subtitle1'
+                                            sx={{ fontWeight: 'bold' }}
+                                            mr={2}>
+                                            Birthday
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography
+                                            variant='body1'>
+                                            {dayjs(goalkeeper?.birthday).format('DD MMM YYYY').toString()}
+                                        </Typography>
+                                    </Grid>
 
-                                <Grid item xs={3}>
-                                    <Typography
-                                        variant='subtitle1'
-                                        sx={{ fontWeight: 'bold' }}
-                                        mr={2}>
-                                        Phone Number
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography
-                                        variant='body1'>
-                                        {goalkeeper?.phone}
-                                    </Typography>
-                                </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography
+                                            variant='subtitle1'
+                                            sx={{ fontWeight: 'bold' }}
+                                            mr={2}>
+                                            Phone Number
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography
+                                            variant='body1'>
+                                            {goalkeeper?.phone}
+                                        </Typography>
+                                    </Grid>
 
-                                <Grid item xs={8} mb={1}>
+                                    <Grid item xs={8} mb={1}>
+                                        <Typography
+                                            variant='subtitle1'
+                                            sx={{ fontWeight: 'bold' }}>
+                                            Associated Categories
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid
+                                        container
+                                        spacing={{ xs: 1 }}
+                                        columns={{ xs: 8, sm: 8, md: 12 }} mt={1}>
+                                        {categories.length > 0 ?
+                                            categories.map((category) => (
+                                                <Grid item xs={4} md={3}
+                                                    key={category.id}
+                                                    mb={1}
+                                                >
+                                                    <Link
+                                                        component={RouterLink}
+                                                        to={`/categories/${category.id}`}
+                                                        underline="none"
+                                                        color="inherit">
+                                                        <Chip
+                                                            icon={<IoFootball />}
+                                                            label={`${category?.name} ${category?.season}`}
+                                                            onClick={() => { }} />
+                                                    </Link>
+                                                </Grid>
+                                            ))
+                                            : <Box pl={1}>
+                                                No associated categories yet.
+                                            </Box>}
+                                    </Grid>
+                                </Grid>
+                            </Card>
+                        </Grid>
+
+                        <Grid item xs={4} sm={3} md={4} order={{ xs: 1, sm: 2, md: 2 }}>
+                            <Card sx={{ width: "auto" }}>
+                                <Box
+                                    display="flex"
+                                    flexDirection="column"
+                                    justifyContent="center"
+                                    alignItems="center"
+                                    component="img"
+                                    sx={{
+                                        maxHeight: { xs: "100%", sm: "100%", md: 500 },
+                                        maxWidth: { xs: "100%", sm: 350, md: 500 },
+                                    }}
+                                    alt="Example Goalkeeper"
+                                    src={goalkeeper?.picture ? goalkeeper.picture : "https://pressboxonline.com/wp-content/uploads/2021/01/dan-enos-800x445.jpg"}
+                                />
+                                <Box
+                                    display="flex"
+                                    flexDirection="column"
+                                    justifyContent="center"
+                                    alignItems="center"
+                                    mb={1}>
                                     <Typography
-                                        variant='subtitle1'
-                                        sx={{ fontWeight: 'bold' }}>
-                                        Associated Categories
+                                        variant='subtitle2'
+                                        sx={{ fontStyle: 'italic' }}
+                                        ml={1} mt={1}>
+                                        UID: {id}
                                     </Typography>
-                                </Grid>
+                                    <Button component="label">
+                                        Change Picture
+                                        <input
+                                            hidden
+                                            accept="image/*"
+                                            multiple type="file"
+                                            onChange={e => uploadPicture(e)} />
+                                    </Button>
 
-                                <Grid
-                                    container
-                                    spacing={{ xs: 1 }}
-                                    columns={{ xs: 8, sm: 8, md: 12 }} mt={1}>
-                                    {categories.length > 0 ?
-                                        categories.map((category) => (
-                                            <Grid item xs={4} md={3}
-                                                key={category.id}
-                                                mb={1}
-                                            >
-                                                <Link
-                                                    component={RouterLink}
-                                                    to={`/categories/${category.id}`}
-                                                    underline="none"
-                                                    color="inherit">
-                                                    <Chip
-                                                        icon={<IoFootball />}
-                                                        label={`${category?.name} ${category?.season}`}
-                                                        onClick={() => { }} />
-                                                </Link>
-                                            </Grid>
-                                        ))
-                                        : <Box pl={1}>
-                                            No associated categories yet.
-                                        </Box>}
-                                </Grid>
-                            </Grid>
-                        </Card>
+                                </Box>
 
-                        <Typography fontWeight="bold" mt={2} mb={1}>Match performances</Typography>
-                        {rows.length > 0 ?
-                            < DataGrid
-                                rows={rows || []}
-                                columns={columns}
-                                pageSize={3}
-                                rowsPerPageOptions={[3]}
-                            /> : <></>
-                        }
+                            </Card>
+                        </Grid>
                     </Grid>
 
-                    <Grid item xs={4} sm={3} md={4} order={{ xs: 1, sm: 2, md: 2 }}>
-                        <Card sx={{ width: "auto" }}>
-                            <Box
-                                display="flex"
-                                flexDirection="column"
-                                justifyContent="center"
-                                alignItems="center"
-                                component="img"
-                                sx={{
-                                    maxHeight: { xs: "100%", sm: "100%", md: 500 },
-                                    maxWidth: { xs: "100%", sm: 350, md: 500 },
-                                }}
-                                alt="Example Goalkeeper"
-                                src={goalkeeper?.picture ? goalkeeper.picture : "https://pressboxonline.com/wp-content/uploads/2021/01/dan-enos-800x445.jpg"}
-                            />
-                            <Box
-                                display="flex"
-                                flexDirection="column"
-                                justifyContent="center"
-                                alignItems="center"
-                                mb={1}>
-                                <Typography
-                                    variant='subtitle2'
-                                    sx={{ fontStyle: 'italic' }}
-                                    ml={1} mt={1}>
-                                    UID: {id}
-                                </Typography>
-                                <Button component="label">
-                                    Change Picture
-                                    <input
-                                        hidden
-                                        accept="image/*"
-                                        multiple type="file"
-                                        onChange={e => uploadPicture(e)} />
-                                </Button>
-
-                            </Box>
-
-                        </Card>
-                    </Grid>
-                </Grid>
+                    <Typography fontWeight="bold" mt={2} mb={1}>Match performances</Typography>
+                    {rows.length > 0 ?
+                        < DataGrid
+                            rows={rows || []}
+                            columns={columns}
+                            pageSize={3}
+                            rowsPerPageOptions={[3]}
+                        /> : <></>
+                    }
+                </>
             }
         </>
 

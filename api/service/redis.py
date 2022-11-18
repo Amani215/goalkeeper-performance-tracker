e592249791e0f89cf975d60e10b3d11a161d5115
Amani215 <amanibrik5@gmail.com>
2022-11-18 15:36:23 +0000

    Added: redis service

---
+++ api/service/redis.py
@@ -0,0 +1,30 @@
+"""Redis related services"""
+from config.redis import redis_db
+
+NO_SUCH_KEY = 'No such key exists'
+
+
+def add_item(key: str, new_value: str):
+    '''Add an item to a redis key'''
+    if redis_db.exists(key) > 0:
+        redis_db.sadd(key, new_value)
+        return redis_db.smembers(key)
+    else:
+        return {'error': NO_SUCH_KEY}
+
+
+def get_items(key: str):
+    '''Get items given the key'''
+    if redis_db.exists(key) > 0:
+        return redis_db.smembers(key)
+    else:
+        return {'error': NO_SUCH_KEY}
+
+
+def delete_item(key: str, value: str):
+    '''Delete an item from a set given its key'''
+    if redis_db.exists(key) > 0:
+        redis_db.srem(key, value)
+        return redis_db.smembers(key)
+    else:
+        return {'error': NO_SUCH_KEY}

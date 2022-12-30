4b113c0328f7738246a86e105606df3654cd55a2
Amani215 <amanibrik5@gmail.com>
2022-12-30 17:11:18 +0000

    Fixed: vultr region

--- terraform/modules/vultr/instance/main.tf
+++ terraform/modules/vultr/instance/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 resource "vultr_instance" "gpt_instance" {
   plan             = "vc2-2c-4gb"
-  region           = "Frankfurt"
+  region           = "fra"
   app_id           = "37"
   ssh_key_ids      = ["4494b9bc-993d-48f5-8205-e598be4a21ce", "706dbac8-0735-435f-8e99-8a74b2ff9711"]
   enable_ipv6      = true

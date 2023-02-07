3cea79dc8bf47c68cefde55f522449e46e1670b0
Amani215 <amanibrik5@gmail.com>
2023-02-07 16:20:23 +0000

    Fixed: trainings UI

--- web-ui/src/containers/trainingsView.tsx
+++ web-ui/src/containers/trainingsView.tsx
@@ -1,22 +1,51 @@
+import { TextField } from '@mui/material'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import React from 'react'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs, { Dayjs } from 'dayjs'
+import React, { useState } from 'react'
+import { GrafanaPanel } from '../components/grafana'
 import { ModalProp } from '../interfaces/modalProp'
 
 function TrainingsView({ setModalIsOpen }: ModalProp) {
+    const [date, setDate] = useState<Dayjs>(dayjs())
+
     return (
         <>
             <Box
                 display="flex"
-                justifyContent="center"
-                alignItems="center"
+                justifyContent={{ xs: 'center', sm: 'space-between' }}
+                alignItems={{ xs: 'center', sm: "baseline" }}
+                sx={{ flexWrap: 'wrap-reverse' }}
                 mb={3}>
-                <Button
-                    variant="contained"
-                    onClick={() => { setModalIsOpen() }}
-                >Add Training
-                </Button>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        views={['year', 'month']}
+                        label="Year and Month"
+                        minDate={dayjs('01/01/2012', 'DD/MM/YYYY')}
+                        maxDate={dayjs('01/12/2050', 'DD/MM/YYYY')}
+                        value={date}
+                        onChange={(v) => {
+                            setDate(dayjs(v, "MM/DD/YYYY"))
+                        }}
+                        renderInput={(params) => <TextField {...params} helperText={null} />}
+                    />
+                </LocalizationProvider>
+
+                <Box
+                    mb={{ xs: 2 }}
+                    ml={{ sm: 2 }}
+                >
+                    <Button
+                        variant="contained"
+                        onClick={() => { setModalIsOpen() }}
+                    >Add Training
+                    </Button>
+                </Box>
             </Box>
+
+            <GrafanaPanel src={`http://localhost/grafana/d-solo/trainLite/trainings?from=${Math.floor(new Date(date.year().toString() + "." + (date.month() + 1).toString() + ".01").getTime())}&to=${Math.floor(new Date(date.year().toString() + "." + (date.month() + 1).toString() + "." + date.daysInMonth()).getTime())}&orgId=1&panelId=12`} xs={12} height={600} />
         </>
     )
 }
--- web-ui/src/pages/trainings/index.tsx
+++ web-ui/src/pages/trainings/index.tsx
@@ -1,9 +1,4 @@
-import { Box, TextField } from '@mui/material'
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import dayjs, { Dayjs } from 'dayjs'
 import { useState } from 'react'
-import { GrafanaPanel } from '../../components/grafana'
 import NewTraining from '../../containers/modals/newTraining'
 import PortalPage from '../../containers/portalPage'
 import TrainingsView from '../../containers/trainingsView'
@@ -11,9 +6,6 @@ import CategoriesProvider from '../../contexts/categoriesContext'
 import TrainingsProvider from '../../contexts/trainingsContext'
 
 function Trainings() {
-    const [date, setDate] = useState<Dayjs>(dayjs())
-
-
     const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
     const handleOpen = () => setModalIsOpen(true)
     const handleClose = () => setModalIsOpen(false)
@@ -26,29 +18,6 @@ function Trainings() {
 
             <PortalPage>
                 <TrainingsView {...{ modalIsOpen, setModalIsOpen: handleOpen }} />
-
-                <Box
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    mt={2}
-                    mb={1}>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                            views={['year', 'month']}
-                            label="Year and Month"
-                            minDate={dayjs('01/01/2012', 'DD/MM/YYYY')}
-                            maxDate={dayjs('01/12/2050', 'DD/MM/YYYY')}
-                            value={date}
-                            onChange={(v) => {
-                                setDate(dayjs(v, "MM/DD/YYYY"))
-                            }}
-                            renderInput={(params) => <TextField {...params} helperText={null} />}
-                        />
-                    </LocalizationProvider>
-                </Box>
-
-                <GrafanaPanel src={`http://localhost/grafana/d-solo/trainLite/trainings?from=${Math.floor(new Date(date.year().toString() + "." + (date.month() + 1).toString() + ".01").getTime())}&to=${Math.floor(new Date(date.year().toString() + "." + (date.month() + 1).toString() + "." + date.daysInMonth()).getTime())}&orgId=1&panelId=12`} xs={12} height={600} />
             </PortalPage>
         </TrainingsProvider>
     )

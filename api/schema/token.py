b7affb9024615b8b19e08c48ae5ac22d3ec0dad6
Amani215 <amanibrik5@gmail.com>
2023-02-22 18:44:42 +0000

    Update: token expiry in environment vars

--- api/schema/token.py
+++ api/schema/token.py
@@ -1,12 +1,13 @@
 """JWT schema"""
 from datetime import datetime,timedelta
 from copy import deepcopy
+import os
 
 class TokenSchema:
     """This class defines the expected structure of the JWT"""
     public_id = str
     exp = datetime
-    def __init__(self,_public_id:str="",_exp:datetime =datetime.utcnow() + timedelta(minutes=45)):
+    def __init__(self,_public_id:str="",_exp:datetime =datetime.utcnow() + timedelta(seconds=int(os.getenv("TOKEN_EXPIRY_IN_SEC")))):
         self.public_id =_public_id
         self.exp=_exp
 

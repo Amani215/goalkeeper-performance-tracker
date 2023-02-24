324b76744313b472635c8f161cb301fab05fa8b9
Amani215 <amanibrik5@gmail.com>
2023-02-24 14:41:03 +0000

    Added: delete match button

--- web-ui/src/components/matchesList/index.tsx
+++ web-ui/src/components/matchesList/index.tsx
@@ -1,7 +1,8 @@
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { MatchDTO } from "../../DTOs/MatchDTO";
-import { Box, Card, Link, Typography } from '@mui/material';
+import { Box, Card, IconButton, Link, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
 
 type PropType = {
     matches: MatchDTO[]
@@ -12,11 +13,22 @@ const columns: GridColDef[] = [
         field: 'date',
         headerName: 'Date',
         flex: 2,
-        minWidth: 80
+        minWidth: 80,
+        renderCell: (params) => {
+            return (
+                <Link
+                    component={RouterLink}
+                    to={`/matches/${params.id}`}
+                    underline="none"
+                    color="inherit">
+                    <Typography>{params.row.date}</Typography>
+                </Link>
+            );
+        }
     },
     {
-        field: 'id',
-        headerName: 'ID',
+        field: 'local',
+        headerName: 'Local',
         flex: 2,
         minWidth: 80,
         renderCell: (params) => {
@@ -26,28 +38,59 @@ const columns: GridColDef[] = [
                     to={`/matches/${params.id}`}
                     underline="none"
                     color="inherit">
-                    <Typography>{params.row.id}</Typography>
+                    <Typography>{params.row.local}</Typography>
                 </Link>
             );
         }
     },
     {
-        field: 'local',
-        headerName: 'Local',
-        flex: 2,
-        minWidth: 80
-    },
-    {
         field: 'visitor',
         headerName: 'Visitor',
         flex: 2,
-        minWidth: 80
+        minWidth: 80,
+        renderCell: (params) => {
+            return (
+                <Link
+                    component={RouterLink}
+                    to={`/matches/${params.id}`}
+                    underline="none"
+                    color="inherit">
+                    <Typography>{params.row.visitor}</Typography>
+                </Link>
+            );
+        }
     },
     {
         field: 'match_type',
         headerName: 'Match Type',
         flex: 2,
-        minWidth: 80
+        minWidth: 80,
+        renderCell: (params) => {
+            return (
+                <Link
+                    component={RouterLink}
+                    to={`/matches/${params.id}`}
+                    underline="none"
+                    color="inherit">
+                    <Typography>{params.row.match_type}</Typography>
+                </Link>
+            );
+        }
+    },
+    {
+        field: 'delete',
+        headerName: 'Delete',
+        flex: 1,
+        minWidth: 30,
+        align: 'center',
+        headerAlign: 'center',
+        renderCell: (params) => {
+            return (
+                <IconButton onClick={() => { console.log("delete match: ", params.row.id) }}>
+                    <MdDelete color='red' />
+                </IconButton>
+            );
+        }
     }
 ];
 
@@ -55,13 +98,15 @@ function MatchesList({ matches }: PropType) {
     return (
         <>
             {matches.length > 0 ?
-                <div style={{ height: 400, width: '100%' }}>
-                    <DataGrid
-                        rows={matches || []}
-                        columns={columns}
-                        pageSize={5}
-                        rowsPerPageOptions={[5]}
-                    />
+                <div style={{ display: 'flex' }}>
+                    <div style={{ height: 400, width: '100%', flexGrow: 1 }}>
+                        <DataGrid
+                            rows={matches || []}
+                            columns={columns}
+                            pageSize={5}
+                            rowsPerPageOptions={[5]}
+                        />
+                    </div>
                 </div> :
                 <Box
                     display="flex"

675e56be834be74a0e7d393aa5638a69b97cf9f0
Amani215 <amanibrik5@gmail.com>
2023-03-13 15:33:44 +0000

    Added: editable growth monitoring service

--- api/service/growth_monitoring.py
+++ api/service/growth_monitoring.py
@@ -1,8 +1,11 @@
 '''Match monitoring services (add, update, etc.)'''
 from datetime import datetime
+import os
 from sqlalchemy.exc import SQLAlchemyError
 from config import db
+from config.redis import redis_db
 from model.growth_monitoring import growth_monitoring
+from model.user import User
 import service.goalkeeper as goalkeeper_service
 
 
@@ -45,9 +48,9 @@ def get_by_goalkeeper_id(goalkeeper_id: str):
         return {'error': str(err)}
 
 
-def update_param(grwoth_monitoring_id: str, param_name: str, param_value: int):
+def update_param(growth_monitoring_id: str, param_name: str, param_value: int):
     '''Update params'''
-    growth_monitoring_obj = get_by_id(grwoth_monitoring_id)
+    growth_monitoring_obj = get_by_id(growth_monitoring_id)
 
     growth_monitoring_obj.__setattr__(param_name, param_value)
 
@@ -59,5 +62,31 @@ def delete(id: str):
     '''Deletes the given growth object from the database'''
     growth_monitoring_obj = get_by_id(id)
 
+    key = f'{id}_editable'
+    redis_db.delete(key)
+
     db.session.delete(growth_monitoring_obj)
     db.session.commit()
+
+
+def editable(gm: growth_monitoring, user: User) -> bool:
+    '''Checks if the user is allowed to edit the given training data'''
+    if user.admin:
+        return True
+
+    key = f'{gm.id}_editable'
+    if redis_db.exists(key) > 0:
+        return redis_db.sismember(key, str(user.id))
+
+    s: set = set()
+    p = redis_db.pipeline()
+    p.multi()
+    for c in gm.goalkeeper.categories:
+        for t in c.trainers:
+            _id = str(t.id)
+            s.add(_id)
+            p.sadd(key, _id)
+
+    p.expire(key, os.getenv('REDIS_CACHE_TTL'))
+    p.execute()
+    return str(user.id) in s

5c9e8da1019149f94d297c8994a19b8596533f2e
Amani215 <amanibrik5@gmail.com>
2023-03-26 20:35:58 +0000

    Added: missing training route tests

--- api/tests/test_training_session_route.py
+++ api/tests/test_training_session_route.py
@@ -6,14 +6,18 @@ import uuid
 import pytest
 
 from helper import random_string, random_date
-from tests.conftest import content_type
 import service.category as category_service
+import service.goalkeeper as goalkeeper_service
 
 URL = '/training_session'
-CATEGORY_URL = '/training_session/category'
 ID_URL = '/training_session?id='
+SESSION_CATEGORY_URL = '/training_session/category'
+PERFORMANCE_URL = '/training_session/performances?id='
 FORM_URL = '/training_session/form?id='
 IMAGE_URL = 'tests/assets/image.jpeg'
+CATEGORY_URL = '/category'
+
+NO_DATA = 'No data was provided'
 
 
 def test_no_token(client):
@@ -52,6 +56,13 @@ def test_get_training_sessions(client, json_headers):
     assert response.status_code == 200
     assert response.json['id'] == training_session_obj.json['id']
 
+    # GET BY CATEGORY ID
+    response = client.get('/training_session?category_id=' + category.id,
+                          headers=json_headers)
+    assert response.status_code == 200
+    assert len(response.json) == 1
+
+    # INVALID ID
     response = client.get(ID_URL + str(uuid.uuid4), headers=json_headers)
     assert response.status_code == 400
     assert 'error' in response.json
@@ -64,11 +75,11 @@ def test_add_training_session(client, json_headers):
         'name': random_string.generate(12),
         'season': random.randint(1500, 2500)
     }
-    category = client.post('/category',
+    category = client.post(CATEGORY_URL,
                            data=json.dumps(category_json),
                            headers=json_headers)
 
-    date = random_date.generate_with_time()
+    date = random_date.generate()
     test_json = {
         'date': date.strftime('%d/%m/%Y'),
         'duration': random.randint(0, 500),
@@ -86,7 +97,7 @@ def test_add_training_session(client, json_headers):
                            data=json.dumps(test_json),
                            headers=json_headers)
     assert response.status_code == 400
-    assert response.json == {'error': 'No data was provided'}
+    assert response.json == {'error': NO_DATA}
 
     date = random_date.generate_with_time()
     test_json = {
@@ -140,7 +151,7 @@ def test_add_training_session(client, json_headers):
 @pytest.mark.parametrize(['admin'], [[True]])
 def test_set_get_category(client, json_headers, category):
     '''Test setting and getting a category to a training session'''
-    date = random_date.generate_with_time()
+    date = random_date.generate()
     test_json = {
         'date': date.strftime('%d/%m/%Y'),
         'duration': random.randint(0, 500),
@@ -160,15 +171,30 @@ def test_set_get_category(client, json_headers, category):
         'training_session_id': str(training_session_obj.json['id']),
         'category_id': category['name'] + str(category['season'])
     }
-    response = client.put(CATEGORY_URL,
+    response = client.put(SESSION_CATEGORY_URL,
                           data=json.dumps(test_data),
                           headers=json_headers)
     assert response.status_code == 201
 
-    response = client.get(CATEGORY_URL + "?id=" +
+    response = client.get(SESSION_CATEGORY_URL + "?id=" +
                           training_session_obj.json['id'],
                           headers=json_headers)
+    assert response.status_code == 200
     assert response.json['id'] == category['name'] + str(category['season'])
+
+    # INVALID ID
+    response = client.get(SESSION_CATEGORY_URL + "?id=" +
+                          random_string.generate(8),
+                          headers=json_headers)
+    assert response.status_code == 400
+
+    # EMPTY JSON
+    test_data = {}
+    response = client.put(SESSION_CATEGORY_URL,
+                          data=json.dumps(test_data),
+                          headers=json_headers)
+    assert response.status_code == 400
+    assert response.json['error'] == NO_DATA
 
 
 @pytest.mark.parametrize(['admin'], [[False]])
@@ -187,4 +213,78 @@ def test_add_form_diff_category(client, authenticated_user, training_session):
     ### BAD JSON
     response = client.put(url, data={}, headers=headers)
     assert response.status_code == 400
-    assert 'No data was provided' in response.json['error']
+    assert NO_DATA in response.json['error']
+
+
+@pytest.mark.parametrize(['admin'], [[True]])
+def test_get_goalkeepers(client, json_headers):
+    '''Test get training session goalkeepers'''
+    category_json = {
+        'name': random_string.generate(12),
+        'season': random.randint(1500, 2500)
+    }
+    category = client.post(CATEGORY_URL,
+                           data=json.dumps(category_json),
+                           headers=json_headers)
+
+    date = random_date.generate()
+    test_json = {
+        'date': date.strftime('%d/%m/%Y'),
+        'duration': random.randint(0, 500),
+        'category_id': category.json['id']
+    }
+    response = client.post(URL,
+                           data=json.dumps(test_json),
+                           headers=json_headers)
+
+    # GET GOALKEEPERS
+    goalkeepers = client.get(PERFORMANCE_URL + response.json['id'],
+                             headers=json_headers)
+    assert goalkeepers.status_code == 200
+    assert len(goalkeepers.json) == 0
+
+
+@pytest.mark.parametrize(['admin'], [[True]])
+def test_remove_goalkeepers(client, json_headers, goalkeeper):
+    '''Test remove a goalkeeper'''
+    category_json = {
+        'name': random_string.generate(12),
+        'season': random.randint(1500, 2500)
+    }
+    category = client.post(CATEGORY_URL,
+                           data=json.dumps(category_json),
+                           headers=json_headers)
+
+    date = random_date.generate()
+    test_json = {
+        'date': date.strftime('%d/%m/%Y'),
+        'duration': random.randint(0, 500),
+        'category_id': category.json['id']
+    }
+    response = client.post(URL,
+                           data=json.dumps(test_json),
+                           headers=json_headers)
+
+    # Add goalkeeper
+    _goalkeeper = goalkeeper_service.get_by_name(goalkeeper['name'])
+    test_json = {
+        'goalkeeper_id': _goalkeeper.id,
+        'session_id': response.json['id']
+    }
+    tp = client.post('/training_monitoring',
+                     data=json.dumps(test_json),
+                     headers=json_headers)
+    goalkeepers = client.get(PERFORMANCE_URL + response.json['id'],
+                             headers=json_headers)
+    assert goalkeepers.status_code == 200
+    assert len(goalkeepers.json) == 1
+
+    test_json = {'goalkeeper_performance_id': tp.json['id']}
+    delete_response = client.delete(PERFORMANCE_URL + response.json['id'],
+                                    data=json.dumps(test_json),
+                                    headers=json_headers)
+    assert delete_response.status_code == 204
+    goalkeepers = client.get(PERFORMANCE_URL + response.json['id'],
+                             headers=json_headers)
+    assert goalkeepers.status_code == 200
+    assert len(goalkeepers.json) == 0

4a6343c5eb82f42198a512fc3c93befdeab01760
Amani215 <amanibrik5@gmail.com>
2023-04-19 15:17:31 +0000

    Added: internationalization

--- web-ui/src/components/header/index.tsx
+++ web-ui/src/components/header/index.tsx
@@ -4,12 +4,29 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import AccountPopover from './AccountPopover';
 import { LoginDTO } from '../../DTOs/LoginDTO';
+import { Switch } from '@mui/material';
+import { useState } from 'react';
+import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 interface IProps {
   auth: LoginDTO | null
 }
 
 export default function Header({ auth }: IProps) {
+  const { t } = useTranslation();
+  const [english, setEnglish] = useState<boolean>(false);
+
+  const changeLanguage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnglish(event.target.checked);
+    if (english) {
+      i18next.changeLanguage('en')
+    } else {
+      i18next.changeLanguage('fr')
+    }
+    console.log(t('test'))
+  };
+
   return (
     <Box sx={{ flexGrow: 1, position: "sticky", top: 0, zIndex: 1 }}>
       <AppBar position="static">
@@ -18,6 +35,10 @@ export default function Header({ auth }: IProps) {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>GPT</Typography>
 
           <div>
+            <Switch
+              checked={english}
+              onChange={changeLanguage}
+              color="default" />
             <AccountPopover
               id={auth?.user ? auth.user.id : ""}
               username={auth?.user ? auth.user.username : ""}

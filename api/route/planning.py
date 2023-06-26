5e469766513f8345e8b4d98159fc2e67f86a4ef9
Amani215 <amanibrik5@gmail.com>
2023-06-26 15:48:40 +0000

    Added: more planning routes

--- api/route/planning.py
+++ api/route/planning.py
@@ -1,6 +1,6 @@
 '''Planning routes'''
 from flask.blueprints import Blueprint
-from flask import jsonify, request
+from flask import request
 
 from middleware.token_required import token_required
 from model.user import User
@@ -10,6 +10,7 @@ import service.category as category_service
 planning_api = Blueprint('planning_api', __name__)
 
 NO_DATA_PROVIDED_MESSAGE = 'No data was provided'
+NOT_EDITABLE_MESSAGE = 'User is not allowed to modify this category'
 
 
 @planning_api.route('/planning', methods=['POST'])
@@ -30,12 +31,90 @@ def add_planning(current_user: User):
 
         category = category_service.get_by_id(category_id)
         if category not in current_user.categories:
-            raise PermissionError(
-                'User is not allowed to modify this category')
+            raise PermissionError(NOT_EDITABLE_MESSAGE)
 
         response = planning_service.add_planning(category_id, date, _type)
 
         return response.serialize, 201
+    except PermissionError as err:
+        return {'error': str(err)}, 401
+    except Exception as err:
+        return {'error': str(err)}, 400
+
+
+@planning_api.route('/planning', methods=['GET'])
+@token_required(admin=False)
+def get_planning(current_user: User):
+    '''
+    Get planning by ID
+    '''
+    try:
+        args = request.args
+        if args.get('id') is None:
+            raise ValueError(NO_DATA_PROVIDED_MESSAGE)
+
+        response = planning_service.get_by_id(args.get('id'))
+
+        return response.serialize, 200
+    except Exception as err:
+        return {'error': str(err)}, 400
+
+
+@planning_api.route('/planning', methods=['PUT'])
+@token_required(admin=False)
+def set_param(current_user: User):
+    '''Set the given param(s) to the given value given the planning object ID
+    
+    Only users from the same category can edit this object'''
+    try:
+        args = request.args
+        if not request.json or args.get('id') is None:
+            raise ValueError(NO_DATA_PROVIDED_MESSAGE)
+
+        _id = args.get('id')
+
+        planning = planning_service.get_by_id(_id)
+        if (planning_service.editable(planning, current_user) == False):
+            raise PermissionError(NOT_EDITABLE_MESSAGE)
+
+        possible_params = [
+            'type', 'techniques', 'physiques', 'psychomotricity', 'tactics',
+            'observation'
+        ]
+        for param in possible_params:
+            if param in request.json:
+                planning = planning_service.update_param(
+                    _id, param, request.json[param])
+
+        # Update date
+        if 'date' in request.json:
+            planning = planning_service.set_date(_id,
+                                                 date=request.json['date'])
+
+        return planning.serialize, 201
+    except PermissionError as err:
+        return {'error': str(err)}, 401
+    except Exception as err:
+        return {'error': str(err)}, 400
+
+
+@planning_api.route('/planning', methods=['DELETE'])
+@token_required(admin=False)
+def delete(current_user: User):
+    '''
+    Delete planning object by ID
+    
+    If ID is not provided an error is raised
+    '''
+    try:
+        args = request.args
+
+        planning = planning_service.get_by_id(args.get('id'))
+        if not planning_service.editable(planning, current_user):
+            raise PermissionError(NOT_EDITABLE_MESSAGE)
+
+        planning_service.delete(args.get('id'))
+        return {}, 204
     except PermissionError as err:
         return {'error': str(err)}, 401
     except Exception as err:
--- api/tests/test_planning_route.py
+++ api/tests/test_planning_route.py
@@ -6,6 +6,9 @@ from helper import random_string, random_date
 import service.user as user_service
 
 URL = '/planning'
+ID_URL = '/planning?id='
+
+NOT_ALLOWED_MESSAGE = 'User is not allowed'
 
 
 @pytest.mark.parametrize(['admin'], [[False]])
@@ -20,7 +23,7 @@ def test_add_planning_unauthorized(client, json_headers, category):
                            data=json.dumps(test_json),
                            headers=json_headers)
     assert response.status_code == 401
-    assert 'User is not allowed' in response.json['error']
+    assert NOT_ALLOWED_MESSAGE in response.json['error']
 
     ### BAD JSON
     test_json = {}
@@ -34,7 +37,7 @@ def test_add_planning_unauthorized(client, json_headers, category):
 @pytest.mark.parametrize(['admin'], [[False]])
 def test_add_planning_authorized(client, json_headers, authenticated_user,
                                  category):
-    '''Test add a planning record without being in the same category'''
+    '''Test add a planning record'''
     user = user_service.get_by_id(authenticated_user['id'])
     user_service.add_category(user, category)
 
@@ -48,3 +51,87 @@ def test_add_planning_authorized(client, json_headers, authenticated_user,
                            headers=json_headers)
     assert response.status_code == 201
     assert 'id' in response.json
+
+
+@pytest.mark.parametrize(['admin'], [[False]])
+def test_get_planning(client, json_headers, planning):
+    '''Test get a planning by ID'''
+    response = client.get(ID_URL + planning.id, headers=json_headers)
+    assert response.json['id'] == planning.id
+    assert response.status_code == 200
+
+    # NO ID
+    response = client.get(URL, headers=json_headers)
+    assert response.status_code == 400
+    assert 'No data was provided' in response.json['error']
+
+
+@pytest.mark.parametrize(['admin'], [[False]])
+def test_update_planning_unauthorized(client, json_headers, planning):
+    '''Test update different params of a planning without authorization'''
+    new_type = random_string.generate(6)
+    new_date = random_date.generate().strftime('%d/%m/%Y')
+    new_tactics = random_string.generate(8)
+    test_json = {'type': new_type, 'date': new_date, 'tactics': new_tactics}
+    response = client.put(ID_URL + planning.id,
+                          data=json.dumps(test_json),
+                          headers=json_headers)
+    assert response.status_code == 401
+    assert NOT_ALLOWED_MESSAGE in response.json['error']
+
+
+@pytest.mark.parametrize(['admin'], [[False]])
+def test_update_planning_authorized(client, json_headers, authenticated_user,
+                                    planning):
+    '''Test update different params of a planning'''
+    user = user_service.get_by_id(authenticated_user['id'])
+    user_service.add_category(user, planning.category)
+
+    new_type = random_string.generate(6)
+    new_date = random_date.generate().strftime('%d/%m/%Y')
+    new_tactics = random_string.generate(8)
+    test_json = {'type': new_type, 'date': new_date, 'tactics': new_tactics}
+    response = client.put(ID_URL + planning.id,
+                          data=json.dumps(test_json),
+                          headers=json_headers)
+    assert response.status_code == 201
+
+    response = client.get(ID_URL + planning.id, headers=json_headers)
+    assert response.json['type'] == new_type
+    assert response.json['date'] == new_date
+    assert response.json['tactics'] == new_tactics
+
+    # NO JSON
+    response = client.put(ID_URL + planning.id,
+                          data=json.dumps({}),
+                          headers=json_headers)
+    assert response.status_code == 400
+
+    # NO ID
+    response = client.put(URL,
+                          data=json.dumps(test_json),
+                          headers=json_headers)
+    assert response.status_code == 400
+
+
+@pytest.mark.parametrize(['admin'], [[False]])
+def test_delete_planning_unauthorized(client, json_headers, planning):
+    '''Test delete a planning without authorization'''
+    response = client.delete(ID_URL + planning.id, headers=json_headers)
+    assert response.status_code == 401
+    assert NOT_ALLOWED_MESSAGE in response.json['error']
+
+
+@pytest.mark.parametrize(['admin'], [[False]])
+def test_delete_planning_authorized(client, json_headers, authenticated_user,
+                                    planning):
+    '''Test delete a planning with authorization'''
+    user = user_service.get_by_id(authenticated_user['id'])
+    user_service.add_category(user, planning.category)
+    pid = planning.id
+
+    response = client.delete(ID_URL + pid, headers=json_headers)
+    assert response.status_code == 204
+
+    response = client.get(ID_URL + pid, headers=json_headers)
+    assert 'error' in response.json

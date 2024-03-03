1c24ad764eb732b202b0fb9448e27a2243f562e6
Amani215 <amanibrik5@gmail.com>
2024-03-03 15:37:40 +0100

    fix: routing bug

--- api/app.py
+++ api/app.py
@@ -64,6 +64,10 @@ def create_app():
     def index():
         return app.send_static_file('index.html')
 
+    @app.errorhandler(404)   
+    def not_found(e):   
+        return app.send_static_file('index.html')
+
     return app
 
 

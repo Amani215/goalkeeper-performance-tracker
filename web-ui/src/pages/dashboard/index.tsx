deeac7c6d4b813e5b15626943fde57bf85b07441
Amani215 <amanibrik5@gmail.com>
2024-04-23 19:44:54 +0200

    fix: grafana dashboard in UI

--- web-ui/src/pages/dashboard/index.tsx
+++ web-ui/src/pages/dashboard/index.tsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     <UserProvider>
       <PortalPage>
-        <GrafanaDashboard src={'http://localhost/grafana/d/mainLite/main-dashboard?orgId=1&from=1667257200000&to=1669849199999&kiosk'} xs={0} height={700} />
+        <GrafanaDashboard src={'http://grafana.localhost/grafana/d/mainLite/main-dashboard?orgId=1&from=1667257200000&to=1669849199999&kiosk'} xs={0} height={700} />
       </PortalPage>
     </UserProvider>
   )

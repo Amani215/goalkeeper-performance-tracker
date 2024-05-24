f3703c0c7cc9e2aac3dd70cfdd74aaa662f738ad
Amani215 <amanibrik5@gmail.com>
2024-05-24 20:05:55 +0200

    added: traefik tls

--- terraform/modules/docker_gpt/traefik.tf
+++ terraform/modules/docker_gpt/traefik.tf
@@ -18,6 +18,7 @@ resource "docker_container" "traefik" {
 
   command = [
     "--providers.docker=true",
+    "--providers.docker.exposedByDefault=false",
     "--api.dashboard=true",
     "--api.debug=true",
     "--metrics=true",
@@ -29,13 +30,40 @@ resource "docker_container" "traefik" {
     "--log.level=WARN",
     "--accesslog=true",
     "--accesslog.filepath=/var/log/traefik/traefik_access.log",
-    "--accesslog.format=json"
+    "--accesslog.format=json",
+
+    "--certificatesresolvers.staging.acme.email=${var.traefik_email}",
+    "--certificatesresolvers.staging.acme.storage=/etc/traefik/certs/staging.json",
+    "--certificatesresolvers.staging.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory",
+    "--certificatesresolvers.staging.acme.dnschallenge=true",
+    "--certificatesresolvers.staging.acme.dnschallenge.provider=digitalocean",
+
+    "--certificatesresolvers.prod.acme.email=${var.traefik_email}",
+    "--certificatesresolvers.prod.acme.storage=/etc/traefik/certs/prod.json",
+    "--certificatesresolvers.prod.acme.caserver=https://acme-v02.api.letsencrypt.org/directory",
+    "--certificatesresolvers.prod.acme.dnschallenge=true",
+    "--certificatesresolvers.prod.acme.dnschallenge.provider=digitalocean",
+
+    "--entryPoints.web.address=:80",
+    "--entrypoints.web.http.redirections.entrypoint.to=websecure",
+    "--entryPoints.web.http.redirections.entrypoint.scheme=https",
+    "--entryPoints.websecure.address=:443",
+    "--entrypoints.websecure.asDefault=true",
+    "--entrypoints.websecure.http.tls.certresolver=${var.traefik_ca_resolver}",
+    "--entrypoints.websecure.http.tls.domains[0].main=${var.domain}",
+    "--entrypoints.websecure.http.tls.domains[0].sans=*.${var.domain}"
   ]
 
   volumes {
     container_path = "/var/run/docker.sock"
     host_path      = "/var/run/docker.sock"
     read_only      = true
+  }
+
+  volumes {
+    container_path = "/etc/traefik/certs"
+    host_path      = "/etc/traefik/certs"
+    read_only      = false
   }
 
   volumes {
@@ -55,8 +83,18 @@ resource "docker_container" "traefik" {
   }
 
   labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
     label = "traefik.http.routers.traefik.rule"
     value = "Host(`traefik.${var.domain}`)"
+  }
+
+  labels {
+    label = "traefik.http.routers.traefik.service"
+    value = "api@internal"
   }
 
   labels {
@@ -69,9 +107,13 @@ resource "docker_container" "traefik" {
     value = "${var.traefik_username}:${bcrypt(var.traefik_password)}"
   }
 
-
   labels {
     label = "traefik.http.routers.traefik.middlewares"
     value = "basicauth@docker"
   }
+
+  env = [
+    "DO_AUTH_TOKEN=${var.do_dns_token}"
+  ]
+
 }
--- terraform/modules/docker_gpt/variables.tf
+++ terraform/modules/docker_gpt/variables.tf
@@ -14,6 +14,21 @@ variable "traefik_password" {
   sensitive = true
 }
 
+variable "traefik_email" {
+  type    = string
+  default = "amani@example.com"
+}
+
+variable "traefik_ca_resolver" {
+  type    = string
+  default = "staging"
+}
+
+variable "do_dns_token" {
+  type      = string
+  sensitive = true
+}
+
 variable "docker_network" {
   type    = string
   default = "gpt"

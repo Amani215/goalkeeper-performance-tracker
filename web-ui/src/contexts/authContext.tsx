709a2b32c3ad5ff3004058446671db9efe203f46
Amani215 <amanibrik5@gmail.com>
2024-12-21 16:28:04 +0100

    update: minor improvements

--- web-ui/src/contexts/authContext.tsx
+++ web-ui/src/contexts/authContext.tsx
@@ -21,6 +21,7 @@ export default function AuthProvider(props: Readonly<PropsWithChildren<{}>>) {
     const [auth, setAuth] = useState<LoginDTO | null>(null)
 
     const [authReady, setAuthReady] = useState<boolean>(false)
+
     const logout: VoidDelegate = useCallback(() => {
         localStorage.removeItem("loginDTO")
         setAuth(null)
@@ -29,24 +30,37 @@ export default function AuthProvider(props: Readonly<PropsWithChildren<{}>>) {
     }, [])
 
     useEffect(() => {
-        const localAuth: LoginDTO = JSON.parse(localStorage.getItem("loginDTO") ?? "{}")
-
-        fetch("/api/auth", {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `bearer ${localAuth.token}`
-            },
-        }).then(response => {
-            if (response.status === 200) {
-                setAuth(localAuth)
-                setAuthReady(true)
-                return
+        const initializeAuth = async () => {
+            const localAuth: LoginDTO = JSON.parse(localStorage.getItem("loginDTO") ?? "{}")
+            if (!localAuth) {
+                setAuthReady(true); // No auth data found
+                return;
             }
-            setAuthReady(true)
-            localStorage.removeItem("loginDTO")
-        })
+
+            try {
+                const response = await fetch("/api/auth", {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `bearer ${localAuth.token}`
+                    },
+                });
+
+                if (response.ok) {
+                    setAuth(localAuth); // token is valid
+                } else {
+                    localStorage.removeItem("loginDTO"); // invalid token. remove it
+                }
+            } catch (error) {
+                console.log("Error while validating the token: ", error);
+            } finally {
+                setAuthReady(true);
+            }
+        }
+
+        initializeAuth();
     }, [])
+
     return (
         <authContext.Provider value={auth}>
             <authReadyContext.Provider value={authReady}>

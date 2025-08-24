27b660622ef6cdca0a4545f2147d1d6fc1a26038
Amani215 <amanibrik5@gmail.com>
2025-08-24 13:25:22 +0200

    added: login page

---
+++ web-ui-new/src/lib/components/login-form.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { cn, type WithElementRef } from '$lib/utils.js';
+	import type { HTMLFormAttributes } from 'svelte/elements';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: WithElementRef<HTMLFormAttributes> = $props();
+
+	const id = $props.id();
+</script>
+
+<form class={cn('flex flex-col gap-6', className)} bind:this={ref} {...restProps}>
+	<div class="flex flex-col items-center gap-2 text-center">
+		<h1 class="text-2xl font-bold">Login to your account</h1>
+		<p class="text-muted-foreground text-balance text-sm">
+			Enter your username below to login to your account
+		</p>
+	</div>
+	<div class="grid gap-6">
+		<div class="grid gap-3">
+			<Label for="email-{id}">Username</Label>
+			<Input id="email-{id}" type="text" placeholder="John Doe" required />
+		</div>
+		<div class="grid gap-3">
+			<div class="flex items-center">
+				<Label for="password-{id}">Password</Label>
+				<a href="##" class="ml-auto text-sm underline-offset-4 hover:underline">
+					Forgot your password?
+				</a>
+			</div>
+			<Input id="password-{id}" type="password" required />
+		</div>
+		<Button type="submit" class="w-full">Login</Button>
+		<div class="text-center text-sm">
+			Don&apos;t have an account?
+			<a href="##" class="underline underline-offset-4"> Read more here </a>
+		</div>
+	</div>
+</form>
---
+++ web-ui-new/src/routes/login/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import CircleGauge from '@lucide/svelte/icons/circle-gauge';
+	import LoginForm from '$lib/components/login-form.svelte';
+	import loginBg from '$lib/assets/loginBg.jpg';
+</script>
+
+<div class="grid min-h-svh lg:grid-cols-2">
+	<div class="flex flex-col gap-4 p-6 md:p-10">
+		<div class="flex justify-center gap-2 md:justify-start">
+			<a href="##" class="flex items-center gap-2 font-medium">
+				<div
+					class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+				>
+					<CircleGauge class="size-4" />
+				</div>
+				FootTracer.
+			</a>
+		</div>
+		<div class="flex flex-1 items-center justify-center">
+			<div class="w-full max-w-xs">
+				<LoginForm />
+			</div>
+		</div>
+	</div>
+	<div class="bg-muted relative hidden lg:block">
+		<img
+			src={loginBg}
+			alt="placeholder"
+			class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+		/>
+	</div>
+</div>

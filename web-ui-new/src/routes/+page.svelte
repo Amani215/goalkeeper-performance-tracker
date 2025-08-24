fcdea0bb30e0e897dcd6ec18fe8aa5573c4020b8
Amani215 <amanibrik5@gmail.com>
2025-08-24 12:52:47 +0200

    added: shadcn support

--- web-ui-new/src/routes/+page.svelte
+++ web-ui-new/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Button class="ml-10 mt-5">Click me</Button>

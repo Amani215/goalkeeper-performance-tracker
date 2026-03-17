0eba5aa7055ccced94c69a19b23dc9b8ca74ae3d
Amani215 <amanibrik5@gmail.com>
2026-03-17 20:19:02 +0000

    added: edit sequence dialog

---
+++ web-ui-new/src/lib/components/match-performance-utils/edit-sequence-dialog.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import Input from '../ui/input/input.svelte';
+	import PencilLine from '@lucide/svelte/icons/pencil-line';
+
+	let { num: initialNum, action: initialAction, reaction: initialReaction, result: initialResult, comment: initialComment } = $props();
+
+	let num = $state(initialNum);
+	let action = $state(initialAction);
+    let reaction = $state(initialReaction);
+    let result = $state(initialResult);
+    let comment = $state(initialComment);
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger class=" {buttonVariants({ variant: 'ghost' })}">
+        <Button variant="ghost" size="icon" aria-label="Submit">
+            <PencilLine />
+        </Button>
+    </Dialog.Trigger>
+	<Dialog.Content class="sm:max-w-[425px]">
+		<Dialog.Header>
+			<Dialog.Title>Edit Sequence</Dialog.Title>
+			<Dialog.Description
+				>Edit an existing sequence here. Click save when you're done.</Dialog.Description
+			>
+		</Dialog.Header>
+		<div class="grid gap-4 py-4">
+			<div class="grid grid-cols-4 items-center gap-4">
+				<Label for="num" class="text-right">Num</Label>
+				<Input type="text" id="num" bind:value={num} class="col-span-3" />
+			</div>
+
+			<div class="grid grid-cols-4 items-center gap-4">
+				<Label for="action" class="text-right">Action</Label>
+				<Input type="text" id="action" bind:value={action} class="col-span-3" />
+			</div>
+
+            <div class="grid grid-cols-4 items-center gap-4">
+				<Label for="reaction" class="text-right">Reaction</Label>
+				<Input type="text" id="reaction" bind:value={reaction} class="col-span-3" />
+			</div>
+
+            <div class="grid grid-cols-4 items-center gap-4">
+				<Label for="result" class="text-right">Result</Label>
+				<Input type="text" id="result" bind:value={result} class="col-span-3" />
+			</div>
+
+            <div class="grid grid-cols-4 items-center gap-4">
+				<Label for="comment" class="text-right">Comment</Label>
+				<Input type="text" id="comment" bind:value={comment} class="col-span-3" />
+			</div>
+		</div>
+		<Dialog.Footer>
+			<Button type="submit">Save changes</Button>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
--- web-ui-new/src/lib/components/match-performance-utils/sequence-actions.svelte
+++ web-ui-new/src/lib/components/match-performance-utils/sequence-actions.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import Trash2 from '@lucide/svelte/icons/trash-2';
-	import PencilLine from '@lucide/svelte/icons/pencil-line';
+	import EditSequenceDialog from './edit-sequence-dialog.svelte';
+	import type { Sequence } from './sequence-columns';
 
-	let sequence = $props();
+	export let sequence: Sequence ;
+	console.log(sequence)
 </script>
 
 <div class="flex justify-end gap-2">
-	<Button size="icon" variant="ghost"><PencilLine /></Button>
+	<EditSequenceDialog
+	  num={sequence.num}
+	  action={sequence.action}
+	  reaction={sequence.reaction}
+	  result={sequence.result}
+	  comment={sequence.comment}
+	/>
 
 	<Button size="icon" variant="ghost"><Trash2 /></Button>
 </div>

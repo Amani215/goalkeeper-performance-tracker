684c04f426f805e1883b0b27165a6795baa12416
Amani215 <amanibrik5@gmail.com>
2026-03-17 20:21:07 +0000

    added: delete sequence alert

--- web-ui-new/src/lib/components/match-performance-utils/sequence-actions.svelte
+++ web-ui-new/src/lib/components/match-performance-utils/sequence-actions.svelte
@@ -3,6 +3,8 @@
 	import Trash2 from '@lucide/svelte/icons/trash-2';
 	import EditSequenceDialog from './edit-sequence-dialog.svelte';
 	import type { Sequence } from './sequence-columns';
+	import { buttonVariants } from '$lib/components/ui/button';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 
 	export let sequence: Sequence ;
 	console.log(sequence)
@@ -17,5 +19,23 @@
 	  comment={sequence.comment}
 	/>
 
-	<Button size="icon" variant="ghost"><Trash2 /></Button>
+	<AlertDialog.Root>
+		<AlertDialog.Trigger class={buttonVariants({ variant: 'ghost' })}>
+			<Button variant="ghost" size="icon" aria-label="Submit">
+				<Trash2 />
+			</Button>
+		</AlertDialog.Trigger>
+		<AlertDialog.Content>
+			<AlertDialog.Header>
+				<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+				<AlertDialog.Description>
+					This action cannot be undone. This will permanently delete the record.
+				</AlertDialog.Description>
+			</AlertDialog.Header>
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<AlertDialog.Action>Continue</AlertDialog.Action>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
 </div>
